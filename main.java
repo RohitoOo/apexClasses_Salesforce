2d0b2be79e31acba3c7b2cbec95eb85f7fe59dec
Rohito Bhambhani <bhambhani.ba@gmail.com>
2020-01-05 06:55:42 -0500

    Initial Commit

---
+++ main.java
@@ -0,0 +1,9 @@
+public class rohito {
+    public static void firstMethod(){
+       String[] listTest = new List<String>{'Rohito1', 'Bhambhani1', ''};
+       
+        listTest.set(2, 'third');
+        System.debug(listTest);
+    }
+}
+
